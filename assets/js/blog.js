68925f6387269ad258407f81b0c000238c1b6c6c
ramyabyyu <bintantramy@gmail.com>
2022-07-09 10:59:39 +0700

    Fixing Technology icons and blog item layout

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -29,13 +29,7 @@ function addBlog(event) {
     startDate = document.getElementById("start-date").value,
     endDate = document.getElementById("end-date").value,
     description = document.getElementById("description").value,
-    // Checkboxes
-    nodejs = document.getElementById("nodejs"),
-    reactjs = document.getElementById("reactjs"),
-    nextjs = document.getElementById("nextjs"),
-    typescript = document.getElementById("typescript");
-
-  const errMsg = document.getElementById("error__msg");
+    errMsg = document.getElementById("error__msg");
 
   if (!projectName || !startDate || !endDate || !description) {
     errAlert.style.display = "block";
@@ -50,16 +44,12 @@ function addBlog(event) {
 
   image = URL.createObjectURL(image.files[0]);
 
-  //   Technologies checkboxessss
-  const checkboxes = [nodejs, nextjs, reactjs, typescript];
-
   const blog = {
     projectName,
     description,
     image,
-    startDate,
-    endDate,
-    technologies: checkboxes.filter((c) => c.checked == true),
+    duration: countDuration(new Date(startDate), new Date(endDate)),
+    technologies: filterChecboxChecked(),
   };
 
   blogs.push(blog);
@@ -72,6 +62,25 @@ function addBlog(event) {
   renderBlog();
 }
 
+// Getting checbox checked
+function filterChecboxChecked() {
+  // querry all checkbox inputs
+  const checkboxChecked = document.querySelectorAll(
+    ".checkbox__group input[type='checkbox']:checked"
+  );
+
+  // Array Container
+  let cbValue = [];
+
+  // turn nodelist into array
+  for (let i = 0; i < checkboxChecked.length; i++) {
+    cbValue.push(checkboxChecked[i].value);
+  }
+
+  return cbValue;
+}
+
+// Closing Alert
 function closeAlert() {
   errAlert.style.display = "none";
 }
@@ -83,60 +92,72 @@ function renderBlog() {
   blogContainer.innerHTML = "";
 
   for (let i = 0; i < blogs.length; i++) {
+    const techs = blogs[i].technologies;
     blogContainer.innerHTML += `
-      <div class="blog__items">
-        <a href="./blog-detail.html">
-          <div class="blog__image">
-            <img src="${blogs[i].image}" alt="Blog Image" />
-          </div>
-          <div class="blog__title">
-            <h4 class="truncate">${blogs[i].projectName}</h4>
-            <p class="truncate">
-              ${blogs[i].description}
-            </p>
-          </div>
-          <div class="blog__date">
-            <p class="truncate">Duration : ${countDuration(
-              new Date(blogs[i].startDate),
-              new Date(blogs[i].endDate)
-            )} Month</p>
-          </div>
-        </a>
+    <div class="blog__items">
+      <a href="./blog-detail.html">
+        <div class="blog__image">
+          <img src="${blogs[i].image}" alt="Blog Image" />
+        </div>
+        <div class="blog__header">
+          <h3 class="blog__title">${blogs[i].projectName}</h3>
+          <p class="blog__duration">Duration : ${blogs[i].duration} Month</p>
+        </div>
+        <div class="blog__body">
+          <p class="blog__description truncate">${blogs[i].description}</p>
+        </div>
+        <div class="blog__techs">
+          ${techs[0] ? `<i class="fa-brands fa-${techs[0]}"></i>` : ""}
+          ${techs[1] ? `<i class="fa-brands fa-${techs[1]}"></i>` : ""}
+          ${techs[2] ? `<i class="fa-brands fa-${techs[2]}"></i>` : ""}
+          ${techs[3] ? `<i class="fa-brands fa-${techs[3]}"></i>` : ""}
+          ${techs[4] ? `<i class="fa-brands fa-${techs[4]}"></i>` : ""}
+          ${techs[5] ? `<i class="fa-brands fa-${techs[5]}"></i>` : ""}
+        </div>
         <div class="blog__actions">
           <button type="button" class="action__btn">Edit</button>
           <button type="button" class="action__btn">Delete</button>
         </div>
-      </div>
+      </a>
+    </div>
         `;
   }
 }
 
-const month = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+function renderTechIcons(techs) {
+  const blogTech = document.querySelector(".blog__tech");
 
-// Time Format
-function timeFormat(time) {
-  const date = time.getDate();
-  const monthIndex = time.getMonth();
-  const year = time.getFullYear();
-
-  const hour = time.getHours();
-  const minute = time.getMinutes();
-
-  return `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`;
+  for (let i = 0; i < techs.length; i++) {
+    blogTech.innerHTML += `<i class="fa-brands fa-${techs[i]}"></i>`;
+  }
 }
+
+// const month = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// // Time Format
+// function timeFormat(time) {
+//   const date = time.getDate();
+//   const monthIndex = time.getMonth();
+//   const year = time.getFullYear();
+
+//   const hour = time.getHours();
+//   const minute = time.getMinutes();
+
+//   return `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`;
+// }
 
 // Count Project Duration
 function countDuration(startDate, endDate) {
